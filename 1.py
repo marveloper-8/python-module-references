5ac26dd0dafb7898b6e3912eea5395d9fa7e949c
Joshua Equere <marveloper.8@gmail.com>
2022-02-04 12:13:00 +0100

    capture and restore the state of the random number generator

--- 1.py
+++ 1.py
@@ -8,4 +8,13 @@ import random
 # print(random.random())
 
 # random getstate
-print(random.getstate())
+# print(random.getstate())
+
+# random setstate
+print(random.random())
+
+a = random.getstate()
+print(random.random())
+
+random.setstate(a)
+print(random.random())
