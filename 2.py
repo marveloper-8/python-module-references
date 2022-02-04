f2c9c342f1e44c1950c86279f318f67bb831982a
Joshua Equere <marveloper.8@gmail.com>
2022-02-04 20:20:32 +0100

    delete request

--- 2.py
+++ 2.py
@@ -1,4 +1,5 @@
 # requests
 import requests
 
-print(requests.get('https://w3schools.com/python/demopage.htm').text)
+# print(requests.get('https://w3schools.com/python/demopage.htm').text)
+print(requests.delete('https://w3schools.com/python/demopage.php').text)
