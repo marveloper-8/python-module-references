269c3f5d6afd1b962a3dcca37151d7882c1e939a
Joshua Equere <marveloper.8@gmail.com>
2022-02-04 12:10:27 +0100

    return the current state of the random generator

--- 1.py
+++ 1.py
@@ -1,5 +1,11 @@
 # random seed
 import random
 
-random.seed(10)
-print(random.random())
+# random.seed(10)
+# print(random.random())
+
+# random.seed(10)
+# print(random.random())
+
+# random getstate
+print(random.getstate())
