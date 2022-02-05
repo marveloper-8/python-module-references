36e76f5f7208065e6adc0577f5627e4030f25370
Joshua Equere <marveloper.8@gmail.com>
2022-02-05 02:58:47 +0100

    set the timeout to a particular value to guarantee that the connection will be timed out

--- 2.py
+++ 2.py
@@ -11,4 +11,5 @@ a = "https://w3schools.com/python/demopage.php"
 # print(requests.delete(a, cookies={"favcolor": "Red"}).status_code)
 # print(requests.delete(a, headers={"HTTP_HOST": "MyVeryOwnHost"}).status_code)
 # print(requests.delete(a, proxies={"https": "https://1.1.0.1.:80"}).status_code)
-print(requests.delete(a, stream=True).status_code)
+# print(requests.delete(a, stream=True).status_code)
+print(requests.delete(a, timeout=0.001).text)
