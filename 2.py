1f045d21507ed20eb081f552b49adf9dd91da183
Joshua Equere <marveloper.8@gmail.com>
2022-02-05 02:33:14 +0100

    delete cookies

--- 2.py
+++ 2.py
@@ -5,5 +5,7 @@ import requests
 # print(requests.delete('https://w3schools.com/python/demopage.php').text)
 # print(requests.delete('https://w3schools.com/python/demopage.php').text)
 a = "https://w3schools.com/python/demopage.php"
-print(requests.delete(a, allow_redirects=False).text)
-print(requests.delete(a, auth=('user', 'pass')).status_code)
+# print(requests.delete(a, allow_redirects=False).text)
+# print(requests.delete(a, auth=('user', 'pass')).status_code)
+# print(requests.delete(a, cert='folder/myclient.cert').status_code)
+print(requests.delete(a, cookies={"favcolor": "Red"}).status_code)
