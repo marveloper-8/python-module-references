74680cffe139a516f224f244e6877ce7417160b6
Joshua Equere <marveloper.8@gmail.com>
2022-02-05 01:42:16 +0100

    use the auth paramter to send requests with HTTP  basic auth

--- 2.py
+++ 2.py
@@ -4,4 +4,6 @@ import requests
 # print(requests.get('https://w3schools.com/python/demopage.htm').text)
 # print(requests.delete('https://w3schools.com/python/demopage.php').text)
 # print(requests.delete('https://w3schools.com/python/demopage.php').text)
-print(requests.delete("https://w3schools.com/python/demopage.php", allow_redirects=False).text)
+a = "https://w3schools.com/python/demopage.php"
+print(requests.delete(a, allow_redirects=False).text)
+print(requests.delete(a, auth=('user', 'pass')).status_code)
